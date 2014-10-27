d7cd3edfb6c9ab6b1ce22dd0070260723cd25b01
ebrahim-moshaya <ebrahim.moshaya@newvoicemedia.com>
2014-10-27 09:20:17 +0000

    0.3

--- Bootstrap-EC2-Windows-chef-client.ps1
+++ Bootstrap-EC2-Windows-chef-client.ps1
@@ -2,7 +2,7 @@
 #
 # <powershell>
 # Set-ExecutionPolicy Unrestricted
-# icm $executioncontext.InvokeCommand.NewScriptBlock((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ebrahim-moshaya/ec2bootstrap/master/Bootstrap-EC2-Windows.ps1')) -ArgumentList "AWSAccessKey", "AWSSecretKey"
+# icm $executioncontext.InvokeCommand.NewScriptBlock((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ebrahim-moshaya/ec2bootstrap/master/Bootstrap-EC2-Windows-chef-client.ps1')) -ArgumentList "AWSAccessKey", "AWSSecretKey"
 # </powershell>
 #
 
