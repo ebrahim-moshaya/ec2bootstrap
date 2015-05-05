3d1c729696bd33761a8572777ad2a9ef28aacafb
ebrahim-moshaya <ebrahim.moshaya@newvoicemedia.com>
2015-05-05 10:55:38 +0100

    updated to download new bucket files

--- Bootstrap-EC2-Windows-chef-client.ps1
+++ Bootstrap-EC2-Windows-chef-client.ps1
@@ -36,18 +36,39 @@ while (($AWSSecretKey -eq $null) -or ($AWSSecretKey -eq ''))
   $AWSSecretKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((Read-Host "Enter a non-null / non-empty AWS SecretKey" -AsSecureString)))
 }
 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Detect Processor Architecture and OperatingSystemSKU
+# http://blogs.msdn.com/b/david.wang/archive/2006/03/26/howto-detect-process-bitness.aspx
+# http://msdn.microsoft.com/en-us/library/ms724358.aspx
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+$systemPath = [Environment]::GetFolderPath([Environment+SpecialFolder]::System)
+$sysNative = [IO.Path]::Combine($env:windir, "sysnative")
+
+$Is32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+Add-Content $log -value "Is 32-bit [$Is32Bit]"
+
+
+$coreEditions = @(0x0c,0x27,0x0e,0x29,0x2a,0x0d,0x28,0x1d)
+$IsCore = $coreEditions -contains (Get-WmiObject -Query "Select OperatingSystemSKU from Win32_OperatingSystem" | Select -ExpandProperty OperatingSystemSKU)
+Add-Content $log -value "Is Core [$IsCore]"
+
 # move to home, PS is incredibly complex :)
-cd $ENV:USERPROFILE
-Set-Location -Path $ENV:USERPROFILE
+cd $Env:USERPROFILE
+Set-Location -Path $Env:USERPROFILE
 [Environment]::CurrentDirectory=(Get-Location -PSProvider FileSystem).ProviderPath
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
-#	Function:	Download-Bucket-File
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Download-Bucket-File
 #
-#	Comments:	This function is intended to download a specific file from S3.
+# Comments: This function is intended to download a specific file from S3.
 #
 #
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function Download-Bucket-File ($Filename, $Bucket, $Destination)
 {
   $status = "Downloading " + $Filename + " from S3 [" + $Bucket + "]"
@@ -66,13 +87,15 @@ function Download-Bucket-File ($Filename, $Bucket, $Destination)
   Log_Status $status
   
 }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
-#	Function:	Wait-Until-Downloaded
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Wait-Until-Downloaded
 #
-#	Comments:	This function is intended to wait until a particular file has been downloaded.
+# Comments: This function is intended to wait until a particular file has been downloaded.
 #
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function Wait-Until-Downloaded ($Path)
 {
   While (1 -eq 1) {
@@ -84,53 +107,149 @@ function Wait-Until-Downloaded ($Path)
     Start-Sleep -s 60
   }
 }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
 # Download and Install Chef Client
 # https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/chef-windows-11.16.2-1.windows.msi
 #
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function CHEF
 {
-  # Create a new chef directory
-  $chef_dir = "C:\chef"
-  if (!(Test-Path -path $chef_dir))
+  $destchefDir = 'c:\\chef'
+  $desttmpDir = 'c:\\tmp'
+  If (!(Test-Path $destchefDir\\chef_bootstrap.pem))
   {
-    mkdir $chef_dir
+    Log_Status "Running Windows slavesetup script (cwd:  $(pwd) id: $(id))"
+    If (Test-Path $destchefDir\\client.pem)
+    {
+     Log_Status "Removed Packer $destchefDir"
+     Remove-Item -Recurse -Force $destchefDir
+    }
+
+    # Install chef-client if necessary
+    if ( get-command chef-client )
+    {
+      Log_Status "Already Installed $(chef-client -v)"
+    }
+    Else
+    {
+      wget --no-check-certificate http://www.getchef.com/chef/install.msi
+    }
+
+
+    # Copy the files uploaded from the jenkins master into the /etc/chef directory
+    If (!(Test-Path $destchefDir))
+    {
+      mkdir -path $destchefDir
+    }
+    Else
+    {
+      Log_Status "$destchefDir already exists"
+    }
+
+    If (!(Test-Path $destchefDir\\chefsolo.json))
+    {
+      Log_Status "Download bucket Chef bootstrap files onto $destchefDir"
+      Download-Bucket-File "roles.json"  "chefbootstrap-jenkins\msbuildpackage" $chef_dir
+      Download-Bucket-File "validation.pem"  "chefbootstrap-jenkins\msbuildpackage" $chef_dir
+      Download-Bucket-File "solo.rb"  "chefbootstrap-jenkins\msbuildpackage" $chef_dir
+      Download-Bucket-File "chefsolo.json"  "chefbootstrap-jenkins\msbuildpackage" $chef_dir
+      Download-Bucket-File "chef_bootstrap.pem"  "chefbootstrap-jenkins\msbuildpackage" $chef_dir
+    }
+    Else
+    {
+      Log_Status "bootstrap files already in $destchefDir"
+    }
+
+    # Download the bootstrap cookbooks
+    If (!(Test-Path $desttmpDir))
+    {
+      mkdir -path $desttmpDir
+    }
+    Else
+    {
+      Log_Status "$desttmpDir already exists"
+    }
+
+    If (!(Test-Path $desttmpDir\\nvm_bootstrap.tar.gz))
+    {
+      Log_Status "downloading nvm_bootstrap.tar.gz"
+      Invoke-WebRequest -Verbose http://nvmchef-bootstrap.s3-website-eu-west-1.amazonaws.com/nvm_bootstrap.tar.gz -OutFile $desttmpDir\\nvm_bootstrap.tar.gz
+      If (!(Test-Path $desttmpDir\\chef-solo))
+      {
+        mkdir -path $desttmpDir\\chef-solo
+      }
+      Else
+      {
+        Log_Status "$desttmpDir\\chef-solo already exists"
+      }
+    }
+    Else
+    {
+      Log_Status "nvm_bootstrap.tar.gz already exists"
+    }
+
+    If (!(Test-Path $desttmpDir\\chef-solo\\cookbooks))
+    {
+      tar -C /cygdrive/c/tmp/chef-solo -xvf /cygdrive/c/tmp/nvm_bootstrap.tar.gz
+    }
+    Else
+    {
+      Log_Status "nvm_bootstrap.tar.gz already extracted"
+    }
+
+    # Ensure the ohai ec2 hints file exists
+    If (!(Test-Path $destchefDir\\ohai\\hints))
+    {
+      Log_Status "Creating hints folder"
+      mkdir -path $destchefDir\\ohai\\hints
+    }
+    Else
+    {
+      Log_Status "hints folder already created"
+    }
+
+    If (!(Test-Path $destchefDir\\ohai\\hints\\ec2.json))
+    {
+      Log_Status "Creating hints file"
+      Log_Status "{}" | Out-File $destchefDir\\ohai\\hints\\ec2.json -Encoding ASCII 
+    }
+    Else
+    {
+      Log_Status "hints file already created"
+    }
+
+    # Bootstrap with chef-solo if the first-boot
+    If (!(Test-Path $destchefDir\\client.pem))
+    {
+      Log_Status "Running chef-solo to bootstrap client"
+      chef-solo --config $destchefDir\\solo.rb --json-attributes $destchefDir\\chefsolo.json 2>&1 | tee $desttmpDir\\chef_solo.log
+    }
+    Else
+    {
+      Log_Status "Client already bootstrapped"
+    }
+
+    Log_Status "Running Chef-Client"
+    chef-client --json-attributes $destchefDir\\roles.json --environment BuildServer-Testing-Cloud3 -l debug
   }
-  else
+  Else
   {
-    Log_Status "Removed chef directory" 
-    Remove-Item -Recurse -Force $chef_dir
-    mkdir $chef_dir
+      Log_Status "Client already bootstrapped"
   }
-  SetX Path "${env:Path};C:\opscode\chef\bin" /m
-  $Env:Path += ';C:\opscode\chef\bin'
-  Log_Status "Created chef directory" 
-  #	Download Chef.rb and validation key
-  Download-Bucket-File "client.rb"  "chefbootstrap-jenkins" $chef_dir
-  Download-Bucket-File "validation.pem"  "chefbootstrap-jenkins" $chef_dir
-  Download-Bucket-File "knife.rb"  "chefbootstrap-jenkins" $chef_dir
-  [Environment]::SetEnvironmentVariable("CHEFNODE", "JenkinsSlave-${env:Computername}", "Machine")
-  "node_name 'JenkinsSlave-${env:ComputerName}'" | out-file -filepath C:\chef\client.rb -append -Encoding UTF8
-  "node_name 'JenkinsSlave-${env:ComputerName}'" | out-file -filepath C:\chef\knife.rb -append -Encoding UTF8
-  cd $chef_dir
-  schtasks /create /tn "chef-client" /tr "C:\opscode\chefdk\bin\chef-client.bat -L c:\chef\chef-client-run.log" /RU SYSTEM /RL HIGHEST /sc onstart
-  chef-client
-  knife node run_list add JenkinsSlave-${env:Computername} 'role[jenkins_windows_slave]' *>&1 | tee -a c:\chef\knife.log
-  chef-client *>&1 | tee -a c:\chef\chef-client-run.log
 }
 
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
-#	Function:	Log_Status
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Function: Log_Status
 #
-#	Comments:	This function is intended to write a status message to SQS queue and / or local log file
+# Comments: This function is intended to write a status message to SQS queue and / or local log file
 #
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function Log_Status ($message)
 {
@@ -139,26 +258,19 @@ function Log_Status ($message)
 }
 
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
-#	Section	:	Script begins here
+# Section : Script begins here
 #
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 Log_Status "Started bootstrapping EC2 Instance"
 
-if (&C:\Windows\System32\schtasks.exe /Query | &C:\Windows\System32\findstr.exe /B /I "chef-client")
-{ 
-  echo "skip"
-}
-else 
-{
-  Log_Status "Configuring Chef-Client" 
-  CHEF
-  Log_Status "Finished configuring chef-client" 
-}
 
-#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+Log_Status "Configuring Chef Client" 
+CHEF
+Log_Status "Finished configuring Chef Client" 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 Log_Status "Finished bootstrapping"
-
